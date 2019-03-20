7f963644eb3f631bee680906ec0ce3c29c82d11e
smk87 <30999133+smk87@users.noreply.github.com>
2019-03-20 21:45:26 +0600

    static files added.

--- blogs/models.py
+++ blogs/models.py
@@ -6,3 +6,12 @@ class Blog(models.Model):
     body = models.TextField()
     pub_date = models.DateField()
     image = models.ImageField(upload_to="images/")
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e ,%y')
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Blog
+
+
+def allblogs(request):
+    blogs = Blog.objects
+    return render(request, 'blogs/allblogs.html', {"blogs": blogs})
+
+
+def detail(request, blog_id):
+    blogDetail = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blogs/detail.html', {'blog': blogDetail})
