4203a9194d69aca37ef224e3c9ff42121dd5ba8a
smk87 <30999133+smk87@users.noreply.github.com>
2019-03-20 18:35:20 +0600

    Homepage added with bootstrap 4.

---
+++ blogs/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    pub_date = models.DateField()
+    image = models.ImageField(upload_to="images/")
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blogs.apps.BlogConfig',
     'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
